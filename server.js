d24a3ca510b63926162e6e87336afccee1cabaa2
Your Name <you@example.com>
2023-05-10 04:42:09 +0300

    added dotenv

---
+++ server.js
@@ -0,0 +1,29 @@
+/*
+- To use .env:
+    import dotenv from 'dotenv';
+    dotenv.config();
+    mongoose.connect(process.env.mongoConnectionURL);
+*/
+
+import express from "express";
+import { engine } from 'express-handlebars';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// const mongoConnectionURL = 'mongodb://localhost:27017/node_project';
+mongoose.connect(process.env.mongoConnectionURL); //==> Instead of: mongoose.connect(mongoConnectionURL);
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './templates');
+
+// const PORT = 5000;
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Started the application on http://localhost: ${process.env.PORT}`);
+    //= console.log('Started the application on http://localhost: '+PORT);
+});
